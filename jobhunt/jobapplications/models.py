dbe164cbe19dd811e4e208bb5ceede5cf8ca59f5
Zharko-C <zharkoc@zharconsulting.com>
2023-01-02 15:14:26 +0000

    Update

--- jobhunt/jobapplications/models.py
+++ jobhunt/jobapplications/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+class Application(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    link = models.URLField(max_length=250, blank=False, unique=True)
+    application_date = models.DateField(default=datetime.date.today(),blank=False)
+    response_date = models.DateField(null=True, blank=True)
+    rejection_date = models.DateField(null = True, blank=True)
+    cv_included = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+    def was_added_recently(self):
+        return self.application_date >= timezone.now() - datetime.timedelta(days=7)
--- jobhunt/jobhunt/env_settings.py
+++
@@ -1,2 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+c*-6p&9pu_pkuq=7z3b*4_++qhihl#uevwx8#q1cohj3lvgly'
